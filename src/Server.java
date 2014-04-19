c7ced8bbe4d01dce9e00e5802b15d0a8f7ba2cfe
zxwelchxz <zxwelchxz@gmail.com>
2014-04-19 13:25:36 -0400

    Client/Server code added

---
+++ src/Server.java
@@ -0,0 +1,43 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Server {
+
+	private static ServerSocket socket = null;
+	
+	private static ArrayList<Socket> socketList = new ArrayList<Socket>(10);
+
+	public static void main(String[] args) throws IOException {
+		int port = 8149;
+		try {
+			socket = new ServerSocket(port);
+
+			while (true) {
+				Socket connection = socket.accept();
+				socketList.add(connection);
+				Runnable runnable = new Client(connection);
+				Thread thread = new Thread(runnable);
+				thread.start();
+			}
+		} 
+		catch (Exception e) {
+			try {
+				socket.close();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+		}
+		finally {
+			socket.close();
+		}
+
+	}
+
+	public static synchronized ArrayList<Socket> getSocketList() {
+		return socketList;
+	}
+}
+
